6a048137f7c053f344a6151731fa77fb2fac20ee
ajbyu <ajames26@byu.edu>
2023-03-22 19:36:41 -0600

    Added lazy css.

--- client/src/team.tsx
+++ client/src/team.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import teams from './ncaa_teams.json'
-
-type TeamProps = {
-    school: string;
-    name: string;
-    city: string;
-    state: string;
-}
+import './team.css';
 
 class Team extends React.Component<{school:string, name:string, city:string, state:string}> {
     render() {
         const theTeam = this.props;
 
         return (
-            <div>
+            <div className="card">
                 <h3>{this.props.school}</h3>
                 <div>Mascot: {this.props.school}</div>
                 <div>Location: {this.props.city}, {this.props.state}</div>
